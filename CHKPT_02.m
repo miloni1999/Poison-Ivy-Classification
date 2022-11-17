ef0318cb663726a972f8ce870050da2f031db912
rasika-1405 <rasika.sasturkar14@gmail.com>
2022-11-17 11:41:42 -0500

    Project draft submission

--- CHKPT_02.m
+++ CHKPT_02.m
@@ -1,22 +1,31 @@
-function Preprocessing(image)
-    im_in = imread(image);
+function CHKPT_02()
+    im_in = imread("IMAGES/IMG_3127.JPG");
     figure, imshow(im_in);
-    im_resized = imresize(im_in, 0.25);
+    disp(size(im_in));
+    im_resized = imresize(im_in, [600, 900]);
     figure, imshow(im_resized);
-%     im_crop = imcrop(im_resized);
+    im_crop = imcrop(im_resized);
+%     imshow(im_crop);
+    im_lab = rgb2lab(im_crop);
+    a_channel = im_lab(:,:,2);
+    figure, imagesc(a_channel);
+%     figure, histogram(a_channel);
+%     mask = a_channel>-2.5 & a_channel<0.5;
+%     a_new = (~mask) == 1;
+%     figure, imshow(a_new);
 %     im_gray = im2gray(im2double(im_resized(:,:,2) - im_resized(:,:,1)));
-    im_gray = im2gray(im2double(im_resized));
-%     im_adjust = imadjust(im_gray);
-%     figure, imshow(im_adjust);
-    thresh = graythresh(im_gray);
-    disp(thresh);
-    im_binarize = imbinarize(im_gray, thresh + 0.06);
-    figure, imshow(im_binarize);
-    se = strel("disk", 16);
-    im_binarize = imclose(im_binarize, se);
-    se = strel('disk', 75);
-    im_binarize = imopen(im_binarize, se);
-    im_binarize = bwareaopen(im_binarize, 3900);
-    figure, imshow(im_binarize);
+%     im_gray = im2gray(im2double(im_resized));
+% %     im_adjust = imadjust(im_gray);
+% %     figure, imshow(im_adjust);
+%     thresh = graythresh(im_gray);
+%     disp(thresh);
+%     im_binarize = imbinarize(im_gray, thresh + 0.06);
+%     figure, imshow(im_binarize);
+%     se = strel("disk", 16);
+%     im_binarize = imclose(im_binarize, se);
+%     se = strel('disk', 75);
+%     im_binarize = imopen(im_binarize, se);
+%     im_binarize = bwareaopen(im_binarize, 3900);
+%     figure, imshow(im_binarize);
 
 end
