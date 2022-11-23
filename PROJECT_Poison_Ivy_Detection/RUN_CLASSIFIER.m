f345c58d1addb2f7fadc158d5dfc2c8b88717acf
ms2111 <ms2111@cs.rit.edu>
2022-11-22 22:19:30 -0500

    final

--- PROJECT_Poison_Ivy_Detection/RUN_CLASSIFIER.m
+++ PROJECT_Poison_Ivy_Detection/RUN_CLASSIFIER.m
@@ -2,12 +2,11 @@ function RUN_CLASSIFIER( )
 in_dir_name  = 'TEST/*.JPG';  % Or whatever
 in_dir = 'TEST';
 % out_dir     = 'Images_classified';
-% vers        = get_fn_version( mfilename() );
 count_P = 0;
 count_NP = 0;
 
 
-%     leaf_classes  = 'PN';
+    leaf_classes  = 'PN';
     load tree_classifier_634.mat tree_classifier; 
 
     training_files = dir(in_dir_name);
@@ -16,11 +15,9 @@ count_NP = 0;
         [~,bn,~] = fileparts( training_files(idx).name );
         fn_in = sprintf('%s%c%s', in_dir,  filesep(), training_files(idx).name );
         disp(fn_in);
-        fn_ot = sprintf('%s%c%s%s', out_dir, filesep(), bn,'.jpg' );
-        
-        im_in     = imread( fn_in );
-%         im_cleaned          = clean_image_vSKELETON( im_in );
-        feats       = get_features_SKELETON( clean_image_vSKELETON( im_in ) );
+        im_in = imread(fn_in);
+        im_cleaned          = clean_image( im_in );
+        feats       = get_features(im_cleaned);
         n_new       = size( feats, 1 );
         if ( n_new <= 0 )
             pred_cls    = 3;   % This blob has no features.  It is cruft.
@@ -28,7 +25,9 @@ count_NP = 0;
             pred_cls    = tree_classifier.predict( feats );
             
         end
+        
         final_predicted = mode(pred_cls);
+        
         if final_predicted == 1
             count_P = count_P + 1;
         else
@@ -38,6 +37,10 @@ count_NP = 0;
         disp("predicted:--------------------------------------------------------")
         disp(final_predicted);
      end
+     
+     disp("Number of Non Poison ivy leaves detected:")
      disp(count_NP);
+     disp("Number of Poison ivy leaves detected:")
      disp(count_P);
+     
 end
